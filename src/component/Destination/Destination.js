fa31a2c0f4a79d83d95961f8f30d53bdd91c492b
Iftekhar Hasan <iftekharhasanrifat@gmail.com>
2021-03-21 15:44:40 +0000

    added datepicker

--- src/component/Destination/Destination.js
+++ src/component/Destination/Destination.js
@@ -3,7 +3,7 @@ import { useParams } from 'react-router';
 import RiderData from '../../RiderData/RiderData'
 import './Destination.css'
 import SimpleMap from '../GoogleMap/SimpleMap';
-import passengerIcon from '../../images/peopleicon.png' 
+import passengerIcon from '../../images/peopleicon.png'
 const Destination = () => {
     const { rideId } = useParams();
     const [destination, setDestination] = useState({
@@ -11,7 +11,7 @@ const Destination = () => {
         pickTo: ''
     })
     const rideInfo = RiderData.find(rider => rider.id === rideId);
-    const {image,name,passenger,price} = rideInfo;
+    const { image, name, passenger, price } = rideInfo;
     const handleBlur = (e) => {
         const newDestination = { ...destination };
         newDestination[e.target.name] = e.target.value;
@@ -39,38 +39,40 @@ const Destination = () => {
                                 </ul>
                             </div>
                             <div className="ride-info mt-3 bg-light p-3">
-                                <img width='55px' src={image} alt=""/>
+                                <img width='55px' src={image} alt="" />
                                 <h4>{name}</h4>
                                 <div className="d-flex align-item-center">
-                                    <img width="28px" height='28px' src={passengerIcon} alt=""/>
+                                    <img width="28px" height='28px' src={passengerIcon} alt="" />
                                     <h4>{passenger}</h4>
                                 </div>
                                 <h4>${price}</h4>
                             </div>
                             <div className="ride-info mt-3 bg-light p-3">
-                                <img width='55px' src={image} alt=""/>
+                                <img width='55px' src={image} alt="" />
                                 <h4>{name}</h4>
                                 <div className="d-flex align-item-center">
-                                    <img width="28px" height='28px' src={passengerIcon} alt=""/>
+                                    <img width="28px" height='28px' src={passengerIcon} alt="" />
                                     <h4>{passenger}</h4>
                                 </div>
                                 <h4>${price}</h4>
                             </div>
                             <div className="ride-info mt-3 bg-light p-3">
-                                <img width='55px' src={image} alt=""/>
+                                <img width='55px' src={image} alt="" />
                                 <h4>{name}</h4>
                                 <div className="d-flex align-item-center">
-                                    <img width="28px" height='28px' src={passengerIcon} alt=""/>
+                                    <img width="28px" height='28px' src={passengerIcon} alt="" />
                                     <h4>{passenger}</h4>
                                 </div>
                                 <h4>${price}</h4>
                             </div>
                         </div>
                         : <form onSubmit={handleSubmit} className="destination-form ml-1 mb-2 bg-light p-3" action="">
-                            <label htmlFor="pickFrom">Pick From</label>
+                            <label htmlFor="pickFrom"><strong>Pick From</strong></label>
                             <input onBlur={handleBlur} name="pickFrom" className="form-control mb-2" type="text" required placeholder="pick from" />
-                            <label htmlFor="pickTo">Pick To</label>
+                            <label htmlFor="pickTo"><strong>Pick To</strong></label>
                             <input onBlur={handleBlur} name="pickTo" className="form-control mb-2" type="text" required placeholder="pick to" />
+                            <label htmlFor="party"><strong>Choose Your Time</strong> </label>
+                            <input type="dateTime-local"  className="form-control mb-2" name="party" id="" />
                             <input className="search-btn btn btn-danger mb-2 p-2" type="submit" value="search" />
                         </form>}
                 </div>
